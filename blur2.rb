6ab154ff47139327c704fda5fed7e5c1f9651eaa
Alyse <awatton89@gmail.com>
2019-08-30 02:03:57 +0000

    fix blur 2

--- blur2.rb
+++ blur2.rb
@@ -3,6 +3,7 @@ class Image
 
 	def initialize(image)
 		@image = image
+		@results = []
 	end
 	
 	def output_image
@@ -13,18 +14,45 @@ class Image
 	end
 
 	def find_ones
-		results = []
+		
 		@image.each_with_index do |row, y|
 			row.each_with_index do |number, x|
 				if number == 1
-					results.push [y,x]
+					@results.push [y,x]
 				end
 			end
 		end
-		return results
 	end
-	
+
+
+	def blur
+
+		@results.each do |y_coordinate, x_coordinate| 
+			
+			#(y-1,x) == top
+			if y_coordinate - 1 >= 0
+				@image[y_coordinate - 1][x_coordinate] = 1
+			end
+
+			#(y+1,x) == bottom
+			if y_coordinate + 1 <= @image.length - 1 
+				@image[y_coordinate + 1][x_coordinate] = 1
+			end
+
+			#(y,x-1) == left
+			if x_coordinate - 1 >= 0
+				@image[y_coordinate][x_coordinate-1] = 1
+			end
+
+			#(y,x+1) == right
+			if x_coordinate + 1 <= @image[0].length - 1
+				@image[y_coordinate][x_coordinate + 1] = 1
+			end
+		end
+	end
 end
+
+
 image = Image.new([
 	[0, 0, 0, 0],
 	[0, 1, 0, 0],
@@ -32,4 +60,11 @@ image = Image.new([
 	[0, 0, 0, 0]
 ])
 
-p image.find_ones
+image.find_ones
+image.blur
+image.output_image
+
+
+
+
+
